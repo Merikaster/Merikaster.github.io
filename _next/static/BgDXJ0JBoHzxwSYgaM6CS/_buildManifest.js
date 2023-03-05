4db6aa8b5877313f1632f0b1a6e842226345eab6
Merikaster <wcl10369@126.com>
2023-03-05 20:29:37 +0800

    update

---
+++ _next/static/BgDXJ0JBoHzxwSYgaM6CS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e03028f18e67ee8b.css","static/css/e40826313bbe9ef6.css","static/chunks/pages/index-3c828c79ecfe1ee1.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
